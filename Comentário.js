f4b3bcc1e20844e71728aaac03699b37e8581a5f
Hikaru <88241844+HikaruTheKat@users.noreply.github.com>
2021-07-31 12:50:23 -0300

    Comentário.js
    Comentando sobre o repositório e projeto

--- Comentário.js
+++ Comentário.js
@@ -4,3 +4,6 @@ Esse repositório será sobre meus códigos em aoi.js e alguns bdb caso vocês q
  
 A hidetaka é uma bot 100% aoi.js mas ela já foi dbd e eu também tenho um canal no YouTube que lá ensinarei sobre aoi.js
 My YouTube Channel: HikaruTheKat 
+
+Tudo que eu ensinarei no projeto e todos códigos que eu postar aqui será totalmente livres e totalmente de graça mas se puder ajudar no canal do YouTube agradeço
+É isso gente todo dia ou a cada 2 dias terá conteúdo novo ou nos códigos ou então no projeto :)
